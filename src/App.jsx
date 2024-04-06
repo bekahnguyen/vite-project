9bf1b203b7fa9c3bd123d2000628c389dcc8a8e9
bekah <bekahritter@gmail.com>
2024-04-05 18:23:14 -0700

    update page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import { puppyList } from './data.js'
-import './App.css'
-import { useState } from 'react'
+import { puppyList } from "./data.js";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log(puppyList);
 
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppyList)
+  function handleClick() {
+    <></>;
+  }
+
   return (
-    <>
-      <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
-      }
-      </div>
-    </>
-  )
+    <div className="App">
+      {featPupId && <p>{featPupId}</p>}
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
