35022922c585d00acdb931ffd57850a960ab7284
bekah <bekahritter@gmail.com>
2024-04-05 19:08:50 -0700

    finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { puppyList } from "./data.js";
 import "./App.css";
 import { useState } from "react";
-
+import "./index.css";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
@@ -11,9 +11,21 @@ function App() {
     <></>;
   }
 
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
   return (
     <div className="App">
-      {featPupId && <p>{featPupId}</p>}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>{featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
       {puppies.map((puppy) => {
         return (
           <p
